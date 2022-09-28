04a8e93fe1be0ad3aa28fb5128b14fc30e2d544d
Bing <bpan2@users.noreply.github.com>
2022-09-28 18:09:18 -0400

    added plugin into index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+
+var plugin = function (options) {
+    var seneca = this;
+    seneca.add({ role: 'product', cmd: 'add' }, function (msg, respond) {
+        this.make('product').data$(msg.data).save$(respond);
+    });
+
+    seneca.add({ role: 'product', cmd: 'get' }, function (msg, respond) {
+        this.make('product').load$(msg.data.product_id, respond);
+    });
+
+    seneca.add({ role: 'product', cmd: 'get-all' }, function (msg, respond) {
+        this.make('product').list$({}, respond);
+    });
+
+    seneca.add({ role: 'product', cmd: 'delete' }, function (msg, respond) {
+        this.make('product').remove$(msg.data.product_id, respond);
+    });
+}
+
+module.exports = plugin;
+
+
 var http = require('http');
 fs = require('fs')
 
