9596ece6814acc9360ac7ba0948986c8c87b35b9
Bing <bpan2@users.noreply.github.com>
2022-09-28 15:33:46 -0400

    modified the program to console out package.json

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var http = require('http');
+fs = require('fs')
+
+fs.readFile('package.json', 'utf8', function(err, data){
+if (err){
+	return console.log(err);
+	}
+console.log(data);
+});
+
+
 var server = http.createServer (function (request, response) {
         response.writeHead (200, {"Content-Type": "text/plain"});
         response.end("Hello World\n");
