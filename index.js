db35f552248c7be48bf6995b7545c386d197f6af
Bing <bpan2@users.noreply.github.com>
2022-09-29 18:52:03 -0400

    the final version for assignment submission

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var count_get = 0
 
 var plugin = function (options) {
     var seneca = this;
@@ -18,7 +19,25 @@ var plugin = function (options) {
         this.make('product').remove$(msg.data.product_id, respond);
     });
 
+    seneca.add(
+        { role: 'product', cmd: 'delete-all' }, function (msg, respond) {
+            var product_entity = this.make('product');
 
+            var product_idList = []
+
+            product_entity.list$({}, function(err,list){
+                list.forEach(function(item){
+                    if ( typeof(item.id) !== "undefined" && item.id !== null ){
+                        console.log("--------------")
+                        console.log(item.productname)
+                        console.log(item.id)
+                        console.log("--------------")
+                        product_entity.remove$(item.id);
+                    }
+                })
+            })            
+        }
+    );
 }
 
 module.exports = plugin;
@@ -30,7 +49,10 @@ seneca.use(plugin);
 seneca.use('seneca-entity');
 
 seneca.add('role:api, cmd:add-product', function (args, done) {
-    console.log("--> cmd:add-product");
+    count_get += 1
+    console.log(" Processed Request Count: " +  " GET: " + count_get);
+    console.log("--> cmd:add-product ");
+   
     var product = {
         productname: args.productname,
         price: args.price,
@@ -44,6 +66,8 @@ seneca.add('role:api, cmd:add-product', function (args, done) {
 });
 
 seneca.add('role:api, cmd:get-all-products', function (args, done) {
+    count_get += 1
+    console.log(" Processed Request Count: " +  " GET: " + count_get);
     console.log("--> cmd:get-all-products");
     seneca.act({ role: 'product', cmd: 'get-all' }, function (err, msg) {
         console.log(msg);
@@ -52,6 +76,8 @@ seneca.add('role:api, cmd:get-all-products', function (args, done) {
 });
 
 seneca.add('role:api, cmd:get-product', function (args, done) {
+    count_get += 1
+    console.log(" Processed Request Count: " +  " GET: " + count_get);
     console.log("--> cmd:get-product, args.user_id: " + args.product_id);
     seneca.act({ role: 'product', cmd: 'get', data: { product_id: args.product_id } }, function (err, msg) {
         console.log(msg);
@@ -61,7 +87,9 @@ seneca.add('role:api, cmd:get-product', function (args, done) {
 
 
 seneca.add('role:api, cmd:delete-product', function (args, done) {
-    console.log("--> cmd:delete-product, args.user_id: " + args.product_id);
+    count_get += 1
+    console.log(" Processed Request Count: " +  " GET: " + count_get);
+    console.log("--> cmd:delete-product, args.product_id: " + args.product_id );
     seneca.act({ role: 'product', cmd: 'delete', data: { product_id: args.product_id } }, function (err, msg) {
         console.log(msg);
         done(err, msg);
@@ -69,8 +97,15 @@ seneca.add('role:api, cmd:delete-product', function (args, done) {
 });
 
 seneca.add('role:api, cmd:delete-all-products', function (args, done) {
-    done(null, { cmd: "delete-all-products" });
+    count_get += 1;
+    console.log(" Processed Request Count: " +  " GET: " + count_get);
+    console.log("--> cmd:delete-all-products");
+    seneca.act({ role: 'product', cmd: 'delete-all'}, function (err, msg) {
+        console.log(msg);
+        done(err, msg);
+    });
 });
+
 
 seneca.act('role:web', {
     use: {
@@ -86,6 +121,7 @@ seneca.act('role:web', {
     }
 })
 
+
 var express = require('express');
 var app = express();
 app.use(require("body-parser").json())
@@ -96,8 +132,13 @@ app.use(seneca.export('web'));
 app.listen(8000)
 console.log("Server listening on localhost:8000 ...");
 console.log("----- Requests -------------------------");
-console.log("http://localhost:8000/bpan4service/add-product?productname=Peter&price=Doe&category=Developer");
+console.log("http://localhost:8000/bpan4service/add-product?productname=Laptop&price=1599&category=PC");
 console.log("http://localhost:8000/bpan4service/get-all-products");
 console.log("http://localhost:8000/bpan4service/get-product?product_id=1245");
 console.log("http://localhost:8000/bpan4service/delete-product?product_id=1245");
 console.log("http://localhost:8000/bpan4service/delete-all-products");
+
+
+
+
+
